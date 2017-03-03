9c2bcc83691b6ade3d4db23366feaf4cf4641d92
Brad <bradcopenhaver@gmail.com>
2017-03-03 10:01:37 -0800

    set up MVC project with views, controller, and models for github api call. define api call method, but it doesn't work yet

---
+++ src/DevPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DevPortfolio.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DevPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public List<ResponseRepo.RootObject> GetRepos(string apiSort)
+        {
+            return GithubRequest.getRepos(apiSort);
+        }
+    }
+}
---
+++ src/DevPortfolio/Models/githubRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using DevPortfolio.Models;
+
+namespace DevPortfolio.Models
+{
+    public class GithubRequest
+    {
+
+        public static List<ResponseRepo.RootObject> getRepos(string sortType)
+        {
+            RestClient client = new RestClient("https://api.github.com");
+            RestRequest request = new RestRequest($"/search/repositories?q=user:bradcopenhaver&sort={sortType}", Method.GET);
+            RestResponse response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            List<ResponseRepo.RootObject> repoResponse = JsonConvert.DeserializeObject<List<ResponseRepo.RootObject>>(jsonResponse["items"].ToString());
+
+            return repoResponse;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
